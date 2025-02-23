1f8ddff8cd3b359e39c281a902fd5bfe3b5b6af3
QiangXinwei <qiangxinwei@outlook.com>
2025-02-23 21:28:17 +0800

    runtime: add intructions print

--- runtime/src/instructions.rs
+++ runtime/src/instructions.rs
@@ -90,3 +90,15 @@ pub enum Instruction {
 
     // }
 }
+
+pub fn print_instructions(instructions: &Vec<Instruction>, spaces: usize) {
+    let prefix = " ".repeat(spaces);
+    for (idx, instruct) in instructions.iter().enumerate() {
+        if let Instruction::Fork { new_thread, instructions } = instruct {
+            println!("{}{}: Fork: {:?}", prefix, idx, new_thread);
+            print_instructions(instructions, spaces + 2);
+        } else {
+            println!("{}{}: {:?}", prefix, idx, instruct);
+        }
+    }
+}
