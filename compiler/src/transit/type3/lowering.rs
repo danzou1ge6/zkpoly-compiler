811811e9462539d5b377f3e344a2e90729137b01
danzou1ge6 <yu_liukun@outlook.com>
2025-03-16 18:59:10 +0800

    修正Type2算子AssertEq生成的指令

--- compiler/src/transit/type3/lowering.rs
+++ compiler/src/transit/type3/lowering.rs
@@ -368,9 +368,15 @@ fn lower_instruction<'s, Rt: RuntimeType>(
                     })
                 }
                 VertexNode::AssertEq(src, truth) => {
+                    assert!(ids.len() == 1);
                     let src = reg_id2var_id(*src);
                     let truth = reg_id2var_id(*truth);
-                    emit(Instruction::AssertEq { value: src, expected: truth })
+                    let out = reg_id2var_id(ids[0]);
+                    emit(Instruction::AssertEq {
+                        value: src,
+                        expected: truth,
+                    });
+                    emit(Instruction::MoveRegister { src, dst: out })
                 }
                 _ => {
                     emit_func::emit_func(
