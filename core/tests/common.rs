dd940e5ddf34ff728d6dbbc7ddf0ed79b722997c
Qiangxinwei <qiangxinwei@outlook.com>
2025-04-21 02:22:14 +0000

    switch back to halo2curves 0.6.0

--- core/tests/common.rs
+++ core/tests/common.rs
@@ -1,4 +1,4 @@
-use halo2curves::{bls12381, bn256};
+use halo2curves::bn256;
 use zkpoly_runtime::args::RuntimeType;
 use zkpoly_runtime::transcript::{Blake2bWrite, Challenge255};
 
@@ -14,12 +14,12 @@ impl RuntimeType for MyRuntimeType {
 
 pub type MyField = <MyRuntimeType as RuntimeType>::Field;
 
-#[derive(Debug, Clone)]
-pub struct BLS12381;
+// #[derive(Debug, Clone)]
+// pub struct BLS12381;
 
-impl RuntimeType for BLS12381 {
-    type Field = bls12381::Fr;
-    type PointAffine = bls12381::G1Affine;
-    type Challenge = Challenge255<Self::PointAffine>;
-    type Trans = Blake2bWrite<Vec<u8>, Self::PointAffine, Challenge255<Self::PointAffine>>;
-}
+// impl RuntimeType for BLS12381 {
+//     type Field = bls12381::Fr;
+//     type PointAffine = bls12381::G1Affine;
+//     type Challenge = Challenge255<Self::PointAffine>;
+//     type Trans = Blake2bWrite<Vec<u8>, Self::PointAffine, Challenge255<Self::PointAffine>>;
+// }
